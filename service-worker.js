c9c9f2c8e521cfd404af5a423927ecf9282f1ded
pkarj <131756544+pkarj@users.noreply.github.com>
2025-07-03 15:19:13 +0800

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -1,21 +1,23 @@
-const cacheName = "music-player-cache-v1";
-const filesToCache = [
-  "./index.html",
-  "./app.js",
-  "./style.css",
-  "./manifest.json",
-  "./icon-192.png",
-  "./icon-512.png"
-];
-
-self.addEventListener("install", e => {
-  e.waitUntil(
-    caches.open(cacheName).then(cache => cache.addAll(filesToCache))
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open("music-player-cache").then((cache) => {
+      return cache.addAll([
+        "./",
+        "./index.html",
+        "./app.js",
+        "./styles.css",
+        "./manifest.json",
+        "./icon-192.png",
+        "./icon-512.png"
+      ]);
+    })
   );
 });
 
-self.addEventListener("fetch", e => {
-  e.respondWith(
-    caches.match(e.request).then(resp => resp || fetch(e.request))
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      return response || fetch(event.request);
+    })
   );
 });
